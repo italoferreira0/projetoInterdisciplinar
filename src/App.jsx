ba7980f451402952067002adfbe40da83602b169
Italo <italofsilva583@gmail.com>
2024-08-21 16:24:05 -0300

    inserção de Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import '../src/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
