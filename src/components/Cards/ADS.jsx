41045ef29b23541e5aa67387e2a4034a4ef30ec5
Italo <italofsilva583@gmail.com>
2024-08-23 15:43:26 -0300

    Inserindo valores apartir do Json

--- src/components/Cards/ADS.jsx
+++ src/components/Cards/ADS.jsx
@@ -2,7 +2,28 @@ import { useState } from "react";
 import '../Cards/ADS.css'
 
 function ADS() {
-
+    const elemento = document.getElementById("nome")
+    const nome = elemento.textContent;
+    
+    function carregar() {
+        fetch("/Dados.json") // buscar arquivo 
+            .then(response => response.json())
+            .then(cursos => {
+                // Filtra o curso
+                const curso = cursos.find(curso => curso.nome === nome);
+                
+                if (curso) {
+                    let paragrafo = document.querySelector("#paragrafo")
+                    paragrafo.innerHTML = `Curso: ${curso.nome} <br>
+                    Duração: ${curso.duracao} <br> Períodos: ${curso.periodos} <br>
+                    Graduação: ${curso.graducao} <br> Instituição: ${curso.Instituicao} `
+                } else {
+                    console.log("Curso não encontrado.");
+                }
+            })
+            .catch(error => console.error("Erro ao carregar os dados:", error));
+    }
+    carregar()
 
     //-----------------------Expandir e Diminuir div ----------------------------------------------------
     const [Expandir, setExpandir] = useState(false);
@@ -19,12 +40,11 @@ function ADS() {
             <div className={Expandir ? 'DivMaximizada' : 'DivMinimizada'} onClick={alterarTamanho}
                 onMouseLeave={recolherDiv}
             >
-                <p>Analise e Desenvolvimento de Sistema</p>
+                <p id="nome">Analise e Desenvolvimento de Sistemas</p>
                 {Expandir && (
                     <div id="conteudo">
-                        <p>Sobre o curso ....</p>
+                        <p id="paragrafo">Sobre o curso ....</p>
 
-                        <p className="TextParagrado"></p>
                     </div>
                 )}
             </div>
