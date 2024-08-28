dfb2096bd6c4cccc967e28ec5da5470d307478ff
Italo <italofsilva583@gmail.com>
2024-08-28 10:03:20 -0300

    Adicionando estilo Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import PagTeste from './Pages/PagTeste';
 function Navbar() {
     return (
         <Router>
-            <nav className="navbar navbar-expand-lg  Custom-navbar">
+            <nav className="navbar navbar-expand-lg Custom-navbar">
                 <div className="container-fluid  ">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -15,7 +15,7 @@ function Navbar() {
                     <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             <Link className="nav-link m-2" to="/" style={{color:"#FFF", fontFamily:"bold",fontSize:"30px"}}>Cursos</Link>
-                            <Link className="nav-link m-2" to="/pagteste" style={{color:"#FFF", fontFamily:"bold",fontSize:"30px"}}>Teste Vocacional</Link>
+                            <Link className="nav-link m-2" to="/teste-vocacional" style={{color:"#FFF", fontFamily:"bold",fontSize:"30px"}}>Teste Vocacional</Link>
                         </div>
                     </div>
                 </div>
@@ -23,7 +23,7 @@ function Navbar() {
 
             <Routes>
                 <Route path="/" element={<PagCursos />} />
-                <Route path="/pagteste" element={<PagTeste />} />
+                <Route path="/teste-vocacional" element={<PagTeste />} />
             </Routes>
         </Router>
     );
