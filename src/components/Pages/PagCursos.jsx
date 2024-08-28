4902c5dc592dddc82fd6987ac45280ba8e9ba26f
Italo <italofsilva583@gmail.com>
2024-08-28 15:59:51 -0300

    Ajustando responsividade dos cards

--- src/components/Pages/PagCursos.jsx
+++ src/components/Pages/PagCursos.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import "../Pages/PagCursos.css"
 
+import Footer from '../Footer';
 
 //Cards--------------------------------------------------------------------
 import ADS from '../Cards/ADS';
@@ -11,35 +12,36 @@ import EngComp from '../Cards/EngComp';
 
 function PagCursos() {
     return (
-        <div className='container' style={{ marginBottom: "20rem" }}>
-            <div className='App'>
-                <Row className='align-items-center Saudacao'>
-                    <Col >
-                        <h1 className='SaudacaoText'>Que tal <br /> <p style={{ color: "white", textShadow: "0 0 5px #fff" }}>
-                            Descobrir</p> sua área</h1>
-                    </Col>
-                </Row>
+        <div>
+            <div className='container PagCursos' >
+                <div className='App'>
+                    <Row className='align-items-center Saudacao'>
+                        <Col >
+                            <h1 className='SaudacaoText'>Que tal <br /> <p style={{ color: "white", textShadow: "0 0 5px #fff" }}>
+                                Descobrir</p> sua área</h1>
+                        </Col>
+                    </Row>
 
-                <Row>
-                    <h1 style={{ textAlign: "center", fontFamily: "'Times New Roman', Times, serif", fontSize: "50px", color: "white" }}
-                    >Cursos</h1>
-                </Row>
-                <Row >
-                    <Col  >
-                        <ADS />
-                    </Col>
-                    <Col >
-                        <Computacao />
-                    </Col>
-                    <Col>
-                        <EngComp />
-                        <SistemasInformacao />
+                    <Row>
+                        <h1 style={{ textAlign: "center", fontFamily: "'Times New Roman', Times, serif", fontSize: "50px", color: "white" }}
+                        >Cursos</h1>
+                    </Row>
+                    <Row >
+                        <Col  >
+                            <ADS />
+                            <SistemasInformacao />
+                        </Col>
+                        <Col >
+                            <Computacao />
+                        </Col>
+                        <Col>
+                            <EngComp />
+                        </Col>
+                    </Row>
 
-                    </Col>
-                </Row>
-                
+                </div>
             </div>
-
+          
         </div>
 
     )
