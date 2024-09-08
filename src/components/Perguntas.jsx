678b2c2150e27c766123f8a2ce35b910d43c43e8
Italo <italofsilva583@gmail.com>
2024-09-08 01:31:40 -0300

    Adicionando estilo no componente Perguntas

--- src/components/Perguntas.jsx
+++ src/components/Perguntas.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import "../components/Perguntas.css"
+
 import BarraProgresso from './BarraProgresso';
 
 function Perguntas() {
@@ -10,42 +12,46 @@ function Perguntas() {
     }
 
     return (
-        <div>
-            <BarraProgresso step={step}/>
-            {step === 0 && (
-                <div>
-                    <p style={{ color: "white" }}>Vamos Iniciar o teste.</p>
-                    <button type="button" onClick={nextStep}>Iniciar</button>
-                </div>
-            )}
-            {step === 1 && (
-                <div>
-                    <p style={{ color: "white" }}>O que você prefere?</p>
-                    <button type="button" onClick={nextStep}>Hardware</button>
-                    <button type="button" onClick={nextStep}>Software</button>
-                </div>
-            )}
+        <div >
+            <BarraProgresso step={step} />
+            <div className='divPergunta'>
+                {step === 0 && (
+                    <div className='divPergunta'>
 
-            {step === 2 && (
-                <div>
-                    <p style={{ color: "white" }}>Você se considera?</p>
-                    <button type="button" onClick={nextStep}>Introvertido</button>
-                    <button type="button" onClick={nextStep}>Extrovertido</button>
-                </div>
-            )}
+                        <div className='Balao'><p className='textPergunta'>Vamos iniciar o teste.</p></div>
+                        
+                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Iniciar</button>
+                    </div>
+                )}
+                {step === 1 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>O que você prefere?</p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Hardware</button>
+                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Software</button>
+                    </div>
+                )}
 
-            {step === 3 && (
-                <div>
-                    <p style={{ color: "white" }}>Você se considera bom em resolver problemas?</p>
-                    <button type="button" onClick={nextStep}>Sim</button>
-                    <button type="button" onClick={nextStep}>Não</button>
-                </div>
-            )}
-            {step === 4 && (
-                <div>
-                    <p style={{ color: "white" }}>Fim do Teste</p>
-                </div>
-            )}
+                {step === 2 && (
+                    <div className='divPergunta'>
+                        <div className='Balao' ><p className='textPergunta'>Você se considera?</p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Introvertido</button>
+                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Extrovertido</button>
+                    </div>
+                )}
+
+                {step === 3 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Você se considera bom em resolver problemas?</p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Sim</button>
+                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Não</button>
+                    </div>
+                )}
+                {step === 4 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Fim do Teste</p></div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
