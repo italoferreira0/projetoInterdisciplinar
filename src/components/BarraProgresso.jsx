fc24161aee6add2e2c08b71cd86495db4caa6a4f
Italo <italofsilva583@gmail.com>
2024-09-21 01:23:00 -0300

    Perguntas Tecnologo

--- src/components/BarraProgresso.jsx
+++ src/components/BarraProgresso.jsx
@@ -3,9 +3,11 @@ import "../components/BarraProgresso.css"
 import { useState, useEffect } from 'react';
 
 function BarraProgresso({step}) {
+    
     const [progresso, setProgresso] = useState(0); 
     useEffect(() => {
-        const novoProgresso = ((step + 0) / 4) * 100;
+        
+        const novoProgresso = ((step + 0) / 7) * 100;
         setProgresso(novoProgresso);
     }, [step]);
 
--- src/components/Perguntas.jsx
+++ src/components/Perguntas.jsx
@@ -6,9 +6,25 @@ import BarraProgresso from './BarraProgresso';
 function Perguntas() {
 
     const [step, setStep] = useState(0);
-
+    const [Tecnologo, setTecnologo] = useState(0);
+    const [Bacharelado, setBacharelado] = useState(0);
+    const [Indiferente, setIndiferente] = useState(0);
+    
     const nextStep = () => {
         setStep(step + 1);
+    }
+
+    const clickTecnologo = () => {
+        setTecnologo(Tecnologo + 1)
+        nextStep()
+    }
+    const clickBacharelado = () => {
+        setBacharelado(Bacharelado + 1)
+        nextStep()
+    }
+    const clickIndiferente = () => {
+        setIndiferente(Indiferente + 1)
+        nextStep()
     }
 
     return (
@@ -19,38 +35,94 @@ function Perguntas() {
                     <div className='divPergunta'>
 
                         <div className='Balao'><p className='textPergunta'>Vamos iniciar o teste.</p></div>
-                        
+
                         <button type="button" className="btn btn-light botoes" onClick={nextStep}>Iniciar</button>
                     </div>
                 )}
                 {step === 1 && (
                     <div className='divPergunta'>
                         <div className='Balao'><p className='textPergunta'>O que você prefere?</p></div>
-                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Hardware</button>
-                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Software</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Tecnólogo</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickBacharelado}>Bacharelado</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickIndiferente}>Indiferente</button>
                     </div>
+
                 )}
 
-                {step === 2 && (
+                {/* Opção Selecionada: Tecnólogo */}
+                {step === 2 && Tecnologo === 1 && (
                     <div className='divPergunta'>
-                        <div className='Balao' ><p className='textPergunta'>Você se considera?</p></div>
-                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Introvertido</button>
-                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Extrovertido</button>
-                    </div>
-                )}
+                        <div className='Balao'><p className='textPergunta'>Quanto tempo voce pretende passar na universidade? </p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>2 anos</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>3 anos</button>
 
-                {step === 3 && (
-                    <div className='divPergunta'>
-                        <div className='Balao'><p className='textPergunta'>Você se considera bom em resolver problemas?</p></div>
-                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Sim</button>
-                        <button type="button" className="btn btn-light botoes" onClick={nextStep}>Não</button>
                     </div>
-                )}
-                {step === 4 && (
+                )
+                }
+
+                {step === 3 && Tecnologo === 2 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Possui interesse em programação? </p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Sim</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Não</button>
+                    </div>
+                )
+                }
+
+                {step === 4 && Tecnologo === 3 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Possui interesse em redes de computadores? </p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Sim</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Não</button>
+                    </div>
+                )
+                }
+
+                {step === 5 && Tecnologo === 4 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Possui interesse em redes de computadores? </p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Sim</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Não</button>
+                    </div>
+                )
+                }
+
+                {step === 6 && Tecnologo === 5 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Você possui interesse em Telecomunicações? </p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Sim</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickTecnologo}>Não</button>
+                    </div>
+                )
+                }
+
+                {step === 7 && Tecnologo === 6 && (
                     <div className='divPergunta'>
                         <div className='Balao'><p className='textPergunta'>Fim do Teste</p></div>
                     </div>
-                )}
+                )
+                }
+
+                {/* Opção Selecionada: Bacharelado */}
+                {step === 2 && Bacharelado === 1 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Você prefere Hardware ou Software?  </p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={clickBacharelado}>Hardware</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickBacharelado}>Software</button>
+                    </div>
+                )
+                }
+
+                {/* Opção Selecionada: Indiferente */}
+                {step === 2 && Indiferente === 1 && (
+                    <div className='divPergunta'>
+                        <div className='Balao'><p className='textPergunta'>Você prefere Hardware ou Software?  </p></div>
+                        <button type="button" className="btn btn-light botoes" onClick={clickIndiferente}>Hardware</button>
+                        <button type="button" className="btn btn-light botoes" onClick={clickIndiferente}>Software</button>
+                    </div>
+                )
+                }
+
             </div>
         </div>
     );
