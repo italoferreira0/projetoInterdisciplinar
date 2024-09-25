68b14da8832c99f092e7df694ca0ff283df89ab6
Italo <italofsilva583@gmail.com>
2024-09-25 01:40:26 -0300

    Ajuste de responsividade nos cards

--- src/components/Pages/PagCursos.jsx
+++ src/components/Pages/PagCursos.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import "../Pages/PagCursos.css"
 
-
-
 //Cards--------------------------------------------------------------------
 import ADS from '../Cards/ADS';
 import Computacao from "../Cards/Computacao"
@@ -11,34 +9,29 @@ import SistemasInformacao from '../Cards/SistemasInformacao';
 import EngComp from '../Cards/EngComp';
 
 function PagCursos() {
-    
+
     return (
         <div>
-            <div className='container PagCursos' >
-                <div className='App'>
-                    <Row className='align-items-center Saudacao'>
-                        <Col >
-                            <h1 className='SaudacaoText'>QUE TAL <br /> <p style={{ color: "white", textShadow: "0 0 5px #fff" }}>
-                                DESCOBRIR</p> SUA ÁREA?</h1>
-                        </Col>
-                    </Row>
-                    <Row >
-                        <Col  >
-                            <ADS />
-                            <SistemasInformacao />
-                        </Col>
-                        <Col >
-                            <Computacao />
-                        </Col>
-                        <Col>
-                            <EngComp />
-                        </Col>
-                    </Row>
-
-                </div>
-            </div>
-          
+    <div className='container PagCursos'>
+        <div className='App'>
+            <Row className='align-items-center Saudacao'>
+                <Col>
+                    <h1 className='SaudacaoText'>
+                        QUE TAL <br />
+                        <p style={{ color: "white", textShadow: "0 0 5px #fff" }}>DESCOBRIR</p>
+                        SUA ÁREA?
+                    </h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={4} sm={12}><ADS /></Col>
+                <Col md={4} sm={12}><SistemasInformacao /></Col>
+                <Col md={4} sm={12}><EngComp /></Col>
+                <Col md={4} sm={12}><Computacao /></Col>
+            </Row>
         </div>
+    </div>
+</div>
 
     )
 }
